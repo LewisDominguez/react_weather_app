c2992bf58725595e00af74128e5d18a4a337d830
unknown <ldominguez1112@gmail.com>
2018-09-26 23:11:29 -0500

    App is able to make an API call to a weather datbase. And user can enter any city and country they want and get the current weather in JSON

--- weather-app/src/App.js
+++ weather-app/src/App.js
@@ -1,11 +1,26 @@
 import React from "react";
 import Titles from "./componets/Titles";
+import Form from "./componets/Form";
+import Weather from "./componets/Weather";
+
+const API_KEY = "4fcd240b2095171a3d5f6f42aba04d33";
 
 class App extends React.Component {
+    getWeather = async (e) => {
+        e.preventDefault();
+        const city = e.target.elements.city.value;
+        const country = e.target.elements.country.value;
+        const api_call = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${API_KEY}`);
+        const data = await api_call.json();
+        console.log(data);
+
+    }
     render() {
         return (
             <div>
                <Titles />
+               <Form getWeather={this.getWeather}/>
+               <Weather />
             </div>
         );
     }
---
+++ weather-app/src/componets/Weather.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+class Weather extends React.Component {
+
+    render() {
+        return(
+            <div>This is the weather Component</div>
+        );
+    }
+};
+
+export default Weather;
